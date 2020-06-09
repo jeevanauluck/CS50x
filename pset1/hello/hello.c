bba3e4443fae07cc8d86e35e9b3c222000bd2a8e
jeevanauluck <jauluck@gmail.com>
2020-06-09 15:05:01 +0000

    finishing touches/comments

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -2,6 +2,6 @@
 #include <cs50.h>
 int main(void)
 {
-    string name = get_string("was ur name?\n");
-    printf("hello, %s\n", name);
+    string name = get_string("What is your name?\n"); //asks for name input
+    printf("Hello, %s\n", name); //placeholder
 }
