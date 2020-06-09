ec2c87bdf4ff023a60cbcd709aeccf900f44959c
jeevanauluck <jauluck@gmail.com>
2020-06-09 14:58:44 +0000

    did mario more comfortable version

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -10,11 +10,19 @@ int main(void)
     while ((n < 1) || (n > 8));
     for (int i = 0; i < n; i++) //loop for printing a new line
     {
-        for (int j = n - i - 1; j > 0; j--) //loop for printing spaces
+        for (int j = n - i - 1; j > 0; j--) //loop for printing spaces left half
         {
             printf(" ");
         }
-        for (int k = 0; k < i + 1; k++) //loop for printing hashes
+        for (int k = 0; k < i + 1; k++) //loop for printing hashes left half
+        {
+            printf("#");
+        }
+        for (int l = 0; l < 1; l++) //loop for printing spaces right half
+        {
+            printf("  ");
+        }
+        for (int m = 0; m < i + 1; m++) //loop for printing hashes right half
         {
             printf("#");
         }
