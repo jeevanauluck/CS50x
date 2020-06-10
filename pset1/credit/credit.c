5a255fc8cd7a81a7199b2b395441a49968a010f1
jeevanauluck <jauluck@gmail.com>
2020-06-10 16:32:51 +0000

    added input checker

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
-
+#include <math.h>
 int main(void)
 {
     long number;
@@ -8,5 +8,7 @@ int main(void)
     {
         number = get_long("Enter card number: ");
     }
-    while
+    while ((floor(log10(number) + 1)) !=13 && (floor(log10(number) + 1)) !=15 && (floor(log10(number) + 1)) !=16); //(ceil((log10(number))) != 2);
+    
+    m
 }
